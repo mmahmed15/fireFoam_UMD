87fd74cf20d6e3d9a61331bb503fc3e1181a2156
Mohamed Ahmed <mmahmed@umd.edu>
2021-10-09 21:55:34 -0400

    added a solver with soot source terms

---
+++ fireFoam_sootEnergy/version.C
@@ -0,0 +1,4 @@
+/*-------------------------------*- C++ -*-----------------------------------*/
+#include "gitVersion.H"
+const char * const build_git_sha = "";
+const char * const build_git_time = "Sat Oct  9 21:54:00 EDT 2021";
