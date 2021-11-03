08da6c1b3c4434276a4694a328ea4bfc89ed853a
Mohamed Ahmed <mmahmed@umd.edu>
2021-11-03 00:57:26 -0400

    added absorption emission diagnostics

--- solver/version.C
+++ solver/version.C
@@ -1,4 +1,4 @@
 /*-------------------------------*- C++ -*-----------------------------------*/
 #include "gitVersion.H"
 const char * const build_git_sha = "47c980c20b13c60c71a0e6d8d562d07c7efd3f58";
-const char * const build_git_time = "Fri Sep 24 21:09:12 PDT 2021";
+const char * const build_git_time = "Thu Oct  7 18:07:54 EDT 2021";
